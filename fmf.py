8c50ca0525e835cafd75ab13a010fd7190ca8ba2
Minibrams <anders@brams.dk>
2019-10-29 15:16:38 +0100

    Functional Matrix Factorization training ready for parallellization

--- fmf.py
+++ fmf.py
@@ -1,8 +1,9 @@
 import torch as tt 
 from data.data_utils import DataUtil, AnswerType
 from os.path import join
-from multiprocessing import Pool
+from multiprocessing import Process, cpu_count
 import math
+from functools import reduce
 
 
 class Tree(): 
@@ -27,6 +28,7 @@ class FunctionalMatrixFactorization():
         ''' 
         self.D = interview_length
         self.k = k
+        self.max_cpu_count = 4
 
     
     def load_data(self, from_file=None): 
@@ -37,45 +39,57 @@ class FunctionalMatrixFactorization():
         self.I = tt.rand((n_items, self.k))
         self.U = tt.rand((n_users, self.k))
 
+    def __get_chunks__(self, l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+
     def __build_node__(self, users=None, node=None, current_depth=None, max_depth=None): 
         print(f'Building node at depth {current_depth}')
-        print()
-        split_item = None 
-        smallest_loss = math.inf
-        n_items = len(self._D.items)
 
-        # This for-loop can be parallellized entirely. We should probably do so.
-        for index, i in enumerate(self._D.items): 
-            uU, uU_group, U_loss = self.__get_optimal_profile_v2__(i, users, answer=AnswerType.UNKNOWN)
-            uL, uL_group, L_loss = self.__get_optimal_profile_v2__(i, users, answer=AnswerType.LIKE)
-            uD, uD_group, D_loss = self.__get_optimal_profile_v2__(i, users, answer=AnswerType.DISLIKE)
+        n_items = len(self.I)
 
-            # Calculate loss for each group 
-            combined_loss = U_loss + L_loss + D_loss
-            if combined_loss < smallest_loss: 
-                smallest_loss = combined_loss
-                split_item = i
-                print(f'Found new item {i} with lowest loss {smallest_loss}')
-                print()
+        partitioned_items = self.__get_chunks__(range(n_items), n_items // self.max_cpu_count)
+        processor_inputs = [(items, users) for items in partitioned_items]
+        # TODO: Parallellize the self.__get_split_loss__() calls below
+        profiles_with_loss = reduce(lambda a,b: a + b, [self.__get_split_loss__(items, users, id=i) for i, (items, users) in enumerate(processor_inputs)])
 
-            print(f'{(index / n_items) * 100 : 2.2f}%', end='\r')
-            
+        print(f'Processed all items...')
+        profiles_with_loss = sorted(profiles_with_loss, key=lambda x: x['loss'])
+        o = profiles_with_loss[0]
+        split_item, uU, uL, uD, split_loss = o['item'], o['unknown'], o['like'], o['dislike'], o['loss']
+        print(f'Best split is on item w. index {split_item} with loss {split_loss}')
 
-        # We are assigned a node with a profile, now add the item and children
-        print(f'Splitting node on item {split_item}')
         node.item = split_item 
         uL_node, uD_node, uU_node = (Tree(profile=uX) for uX in [uL, uD, uU])
         node.children = { answer_type : uX_node for answer_type, uX_node in [(AnswerType.LIKE, uL_node), 
                                                                             (AnswerType.DISLIKE, uD_node), 
                                                                             (AnswerType.UNKNOWN, uU_node)]}
-        # If the tree isn't deep enough yet, keep finding items to split on                                                                     
+        # If the tree isn't deep enough yet, keep finding items to split on
+        uL_group = self._D.get_user_group(users=users, item=split_item, answer=AnswerType.LIKE) 
+        uD_group = self._D.get_user_group(users=users, item=split_item, answer=AnswerType.DISLIKE)   
+        uU_group = self._D.get_user_group(users=users, item=split_item, answer=AnswerType.UNKNOWN)          
+
         if current_depth <= max_depth: 
-            for uX_group, uX, uX_node in [(uL_group, uL_node), 
+            for uX_group, uX_node in [(uL_group, uL_node), 
                                           (uD_group, uD_node), 
                                           (uU_group, uU_node)]: 
                 self.__build_node__(users=uX_group, node=uX_node, current_depth=current_depth + 1, max_depth=max_depth)
         
         return node
+
+    
+    def __get_split_loss__(self, items, users, id=None): 
+        profiles_with_loss = []
+        for index, i in enumerate(items): 
+            print(f'[Thread {id}] currently at {(index / len(items)) * 100 : 2.2f}%', end='\r')
+            uU, uU_group, U_loss = self.__get_optimal_profile_v2__(i, users, answer=AnswerType.UNKNOWN)
+            uL, uL_group, L_loss = self.__get_optimal_profile_v2__(i, users, answer=AnswerType.LIKE)
+            uD, uD_group, D_loss = self.__get_optimal_profile_v2__(i, users, answer=AnswerType.DISLIKE)
+
+            # Calculate loss for each group 
+            profiles_with_loss.append({'unknown' : uU, 'like' : uL, 'dislike' : uD, 'item' : i, 'loss' : U_loss + L_loss + D_loss})
+        
+        return profiles_with_loss
 
 
     def __get_optimal_profile_v2__(self, item, users, answer=None): 
@@ -99,25 +113,6 @@ class FunctionalMatrixFactorization():
         return u, user_indeces, loss.sum()
 
 
-    def __get_optimal_profile__(self, item, users, answer=None): 
-        group = self._D.get_user_group(users=users, item=item, answer=answer)
-        n_users = len(group)
-        coefficient_matrix = tt.zeros((self.k, self.k))
-        coefficient_vector = tt.zeros((1, self.k))
-        for i_u, u in enumerate(group): 
-            for i in self._D.get_rated_items(u):
-                print(f'Coeff matrix: {(i_u / n_users) * 100 : 2.2f}%', end='\r')
-                i_vector = self.I[i].reshape((1, self.k))
-                coefficient_matrix += i_vector.T @ i_vector
-                coefficient_vector += self._D.M[u][i] * self.I[i] 
-
-        print(coefficient_matrix)
-        print(coefficient_matrix.shape)
-        input()
-        coefficient_matrix = tt.inverse(coefficient_matrix)
-        return coefficient_matrix @ coefficient_vector, group
-
-
     def fit(self): 
         self.__build__(len(self._D.items), len(self._D.users))
 
@@ -125,7 +120,7 @@ class FunctionalMatrixFactorization():
         while not has_converged: 
             # 1. Fit a decision tree 
             print(f'Building tree...')
-            tree = self.__build_node__(users=self._D.users, node=Tree(profile=None), current_depth=0, max_depth=self.D)
+            tree = self.__build_node__(users=range(len(self.U)), node=Tree(profile=None), current_depth=0, max_depth=self.D)
             # 2. Fit item embeddings
             break
           
